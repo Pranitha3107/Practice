58ec4445bb1b951f9686c609ca18e9cf1bcb311c
Pranitha3107 <pranithaanumula31@gmail.com>
2025-06-19 07:55:34 +0530

    Challange1

---
+++ src/Test/Challenge1.java
@@ -0,0 +1,15 @@
+package Test;
+public class Challenge1 {
+    public static void main(String[] args) {
+        int i=25;
+        System.out.println(i);
+        double pi=3.14;
+        System.out.println(pi);
+        boolean is_Hanshit_is_aGoodBoy=true;
+        System.out.println(is_Hanshit_is_aGoodBoy);
+            char ch='A';
+        System.out.println(ch);
+
+
+    }
+}
