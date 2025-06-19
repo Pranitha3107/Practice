85fb9c38746e7c32f0a8effc9b38c960cd0f33a0
Pranitha3107 <pranithaanumula31@gmail.com>
2025-06-19 07:58:19 +0530

    Challange2

---
+++ src/Test/Challenge2.java
@@ -0,0 +1,13 @@
+package Test;
+public class Challenge2 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);//dibvison in this case out put is quotient
+        System.out.println(a*b);
+        System.out.println(a%b);//modulo , in thios case out put is reminder
+    }
+
+}
