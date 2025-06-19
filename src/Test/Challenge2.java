e4b374f54d67106ce6d19c1f062dabf13cc23628
Pranitha3107 <pranithaanumula31@gmail.com>
2025-06-19 08:04:24 +0530

    Challange2

--- src/Test/Challenge2.java
+++ src/Test/Challenge2.java
@@ -3,11 +3,12 @@ public class Challenge2 {
     public static void main(String[] args) {
         int a=10;
         int b=5;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a/b);//dibvison in this case out put is quotient
-        System.out.println(a*b);
-        System.out.println(a%b);//modulo , in thios case out put is reminder
-    }
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Modulus:"+(a%b));
+
+         }
 
 }
